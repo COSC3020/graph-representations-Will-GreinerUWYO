63dadb6725c6218e6354415fdd892ef39bec73d0
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-19 10:22:58 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function convertToAdjList(adjMatrix) {
     // need to convert adjMatrix to adjList
     // adjMatrix is [][] where [][] are u,v row = u column = v
-    // adjList is link lists where vertex is head, tail is attached nodes.
+    // adjList is a list of lists where each value in the list is a directed edge between two nodes.
     
     // iterate through adjmatrix n times, where n is number of nodes
     // for each 1, add to the relevant linked list, for one of the numbers
@@ -10,8 +10,8 @@ function convertToAdjList(adjMatrix) {
     let adjList = [];
     for (let row = 0; row < adjMatrix.length; row++) {
         // add a new linked list to the adjList with a header of the row value
-        let newRow = new LinkedList();
-        newRow.add(row);
+        let newRow = [];
+        //let newRow = new LinkedList();
         for (let column = 0; column < adjMatrix.length; column++) {
             if (adjMatrix[row][column] === 1) {
                 newRow.add(column);
@@ -20,35 +20,5 @@ function convertToAdjList(adjMatrix) {
         adjList[row] = newRow;
     }
 
-
-
     return adjList;
-}
-
-function LinkedList() { 
-    this.length = 0; 
-    this.head = null; 
-  
-    var Node = function(element){
-      this.element = element; 
-      this.next = null; 
-    }; 
-  
-    this.add = function(element){
-      var node = new Node(element);
-      if(this.head === null){
-          this.head = node;
-      } else {
-          var currentNode = this.head;
-  
-          while(currentNode.next){
-              currentNode  = currentNode.next;
-          }
-  
-          currentNode.next = node;
-      }
-  
-      this.length++;
-    }; 
-  
 }
