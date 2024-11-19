36e8fa142781cec1ba09984509a4fea05f8132ff
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-18 17:41:22 -0700

    Update code.js

--- code.js
+++ code.js
@@ -11,7 +11,6 @@ function convertToAdjList(adjMatrix) {
     for (let row = 0; row < adjMatrix.length; row++) {
         // add a new linked list to the adjList with a header of the row value
         let newRow = new LinkedList();
-        newRow.add(row);
         for (let column = 0; column < adjMatrix.length; column++) {
             if (adjMatrix[row][column] === 1) {
                 newRow.add(column);
@@ -34,14 +33,6 @@ function LinkedList() {
       this.next = null; 
     }; 
   
-    this.size = function(){
-      return length;
-    };
-  
-    this.head = function(){
-      return head;
-    };
-  
     this.add = function(element){
       var node = new Node(element);
       if(head === null){
@@ -59,98 +50,4 @@ function LinkedList() {
       length++;
     }; 
   
-    this.remove = function(element){
-      var currentNode = head;
-      var previousNode;
-      if(currentNode.element === element){
-          head = currentNode.next;
-      } else {
-          while(currentNode.element !== element) {
-              previousNode = currentNode;
-              currentNode = currentNode.next;
-          }
-  
-          previousNode.next = currentNode.next;
-      }
-  
-      length --;
-    };
-    
-    this.isEmpty = function() {
-      return length === 0;
-    };
-  
-    this.indexOf = function(element) {
-      var currentNode = head;
-      var index = -1;
-  
-      while(currentNode){
-          index++;
-          if(currentNode.element === element){
-              return index;
-          }
-          currentNode = currentNode.next;
-      }
-  
-      return -1;
-    };
-  
-    this.elementAt = function(index) {
-      var currentNode = head;
-      var count = 0;
-      while (count < index){
-          count ++;
-          currentNode = currentNode.next
-      }
-      return currentNode.element;
-    };
-    
-    
-    this.addAt = function(index, element){
-      var node = new Node(element);
-  
-      var currentNode = head;
-      var previousNode;
-      var currentIndex = 0;
-  
-      if(index > length){
-          return false;
-      }
-  
-      if(index === 0){
-          node.next = currentNode;
-          head = node;
-      } else {
-          while(currentIndex < index){
-              currentIndex++;
-              previousNode = currentNode;
-              currentNode = currentNode.next;
-          }
-          node.next = currentNode;
-          previousNode.next = node;
-      }
-      length++;
-    }
-    
-    this.removeAt = function(index) {
-      var currentNode = head;
-      var previousNode;
-      var currentIndex = 0;
-      if (index < 0 || index >= length){
-          return null
-      }
-      if(index === 0){
-          head = currentNode.next;
-      } else {
-          while(currentIndex < index) {
-              currentIndex ++;
-              previousNode = currentNode;
-              currentNode = currentNode.next;
-          }
-          previousNode.next = currentNode.next
-      }
-      length--;
-      return currentNode.element;
-    }
-  
-  } 
+}
